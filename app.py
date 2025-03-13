64548d869d16536ee24d5cfecbb7c79bee2b1c0e
a.dias <arthur.dias@livefr>
2025-03-13 17:48:08 +0100

    Revert "modify to base64 file on upload"
    This reverts commit 2df30b360d5ff8b94763c57e4a59716c608d954a.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
@@ -7,15 +6,11 @@ from transformers import AutoModelForImageClassification, AutoFeatureExtractor
 from werkzeug.utils import secure_filename
 from PIL import Image
 import torch
-import base64
-from io import BytesIO
 
 db_uri = ""
 
 # client = MongoClient(db_uri, server_api=ServerApi('1'))
 app = Flask(__name__)
-CORS(app)
-
 UPLOAD_FOLDER = "uploads"
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 
@@ -43,53 +38,32 @@ def hello_world():  # put application's code here
         print(e)
     return 'Hello World!'
 
-
 @app.route("/upload", methods=["POST"])
 def upload_api():
-    # if request.method == "OPTIONS":
-        # return _build_cors_prelight_response()
-    
-    try:
-        data = request.get_json()
-        if not data or "image" not in data:
-            return jsonify({"error": "Aucune image Base64 reçue"}), 400
+    if "file" not in request.files:
+        return jsonify({"error": "Aucun fichier envoyé"}), 400
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "Fichier vide"}), 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        file.save(file_path)
 
-        base64_image = data["image"]
-        
-        if "," in base64_image:
-            base64_image = base64_image.split(",")[1]
-
-        image_bytes = base64.b64decode(base64_image)
-        image = Image.open(BytesIO(image_bytes)).convert("RGB")
+        image = Image.open(file_path).convert("RGB")
 
         inputs = extractor(images=image, return_tensors="pt")
         outputs = model(**inputs)
         logits = outputs.logits
         predicted_class = torch.argmax(logits, dim=-1)
 
-        emotion_predite = model.config.id2label[predicted_class.item()]
-        print(f"Émotion prédite: {emotion_predite}")
+        os.remove(file_path)
 
-        print(emotion_predite)
+        # Afficher le résultat
+        print(f"Émotion prédite: {model.config.id2label[predicted_class.item()]}")
+        return jsonify({"message": "Fichier uploadé avec succès", "path": file_path, "emotion": model.config.id2label[predicted_class.item()]})
+    return jsonify({"error": "Format de fichier non autorisé"}), 400
 
-        return _corsify_actual_response(jsonify({
-            "message": "Image traitée avec succès",
-            "emotion": emotion_predite
-        }))
-
-    except Exception as e:
-        return _corsify_actual_response(jsonify({"error": str(e)}), 500)
-
-def _build_cors_prelight_response():
-    response = jsonify({"message": "CORS preflight successful"})
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-    response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
-    return response
-
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
 
 
 if __name__ == '__main__':
